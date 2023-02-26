892c2a72370e84f3216354308e06d226fc8b97c7
Douglas Edipo <72673667+DouglasEd@users.noreply.github.com>
2023-02-25 22:13:20 -0300

    Só coloquei excessão rs

--- CPF.py
+++ CPF.py
@@ -1,26 +1,37 @@
 def CPF(ent):
-    ATR = "1234567890" #alfabeto numero de telefone/RG
+    ATR = "1234567890"
     Validade = True
-    entS=[[],[],[],[]]
-    count = 0
-    for j in range(0,4):
-        for i in ent: #Separa a entrada em uma lista
-            if i == "." or i == "-":
-                j=+1
+    entS = []
+    for i in ent:
+        entS.append(i)
+    try:
+        for i in range(0,4):
+            if i < 2:
+                for j in range(0,4):
+                    if j != 3:
+                        if entS[j+(i*4)] not in ATR:
+                            Validade = False
+                    else:
+                        if entS[j+(i*4)] != ".":
+                            Validade = False
+            elif i == 2:
+                for j in range(0,4):
+                    if j != 3:
+                        if entS[j+(i*4)] not in ATR:
+                            Validade = False
+                    else:
+                        if entS[j+(i*4)] != "-":
+                            Validade = False
             else:
-                entS[j].append(i)
-    for j in range(0,4):
-        for i in range(0,len(entS[j])):
-            if entS[j][i] not in ATR: # se algum termo não estiver no alfabelo, ele não aceita
-                Validade = False
-                break
-            if count > 10: # se o tamanho for maior que 11 termos, ele nao aceita
-                Validade = False
-                break
-            count+=1
-    if count < 10: # se o tamanho for menor que 11 termos, ele nao aceita
+                for j in range(12, len(entS)):
+                    if j >= 14:
+                        Validade = False
+                    else:
+                        if entS[j] not in ATR:
+                            Validade = False
+                            break
+    except (RuntimeError, TypeError, NameError):
         Validade = False
-
     if Validade == True:
         print("Entrada aceita")
     else:
--- Main.py
+++ Main.py
@@ -3,19 +3,23 @@ import Telefone,CPF,Email,time
 entS = [] # entrada separada
 
 while True:
-    RM = int(input("""Qual dos identificadores de validade voce quer usar
-    [0] Numero de Telefone
-    [1] CPF
-    [2] Email
-    """))
-    ent = input("Qual o termo que voce quer validar? ")
-    if RM == 0:
-        Telefone.Telefone(ent)
-    elif RM == 1:
-        CPF.CPF(ent)
-    elif RM == 2:
-        Email.Email(ent)
-    else:
-        print("Tu é burro pra krl")
-    time.sleep(10)
+    try:
+        RM = int(input("""Qual dos identificadores de validade voce quer usar
+        [0] Numero de Telefone
+        [1] CPF
+        [2] Email
+        """))
+        ent = input("Qual o termo que voce quer validar? ")
+        if RM == 0:
+            Telefone.Telefone(ent)
+        elif RM == 1:
+            CPF.CPF(ent)
+        elif RM == 2:
+            Email.Email(ent)
+        else:
+            print("Tu é burro pra krl")
+        time.sleep(10)
+    except ValueError:
+        print("Invalido")
+        pass
 
